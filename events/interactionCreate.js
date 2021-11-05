4d8ed07dea7971f900002523ab4b29ac9f1e9923
JannGarcia <jann.garcia@upr.edu>
2021-11-05 11:39:04 -0400

    Added event handling!

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,16 @@
 module.exports = {
 	name: 'interactionCreate',
 	execute(interaction) {
-		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+		const command = interaction.client.commands.get(interaction.commandName);
+		if (!interaction.isCommand()) return;
+		if (!command) return;
+		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction: ${interaction.commandName}`);
+		try {
+			command.execute(interaction);
+		}
+		catch (error) {
+			console.error(error);
+			interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
 	},
 };
--- index.js
+++ index.js
@@ -11,31 +11,26 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+// Read all of the events from the /events directory
+// Only include the .js files
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
+	console.log(command);
 	client.commands.set(command.data.name, command);
 }
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
-});
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	console.log(event);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
 	}
-	catch (error) {
-		console.error(error);
-		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
-});
+}
 
 // Login to Discord with your client's token
 client.login(token);
