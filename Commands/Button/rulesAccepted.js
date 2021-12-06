45b5f14edee258211d62d2c3cae20e3118d14e81
KingFoofle <janngarciapagan@gmail.com>
2021-12-05 22:05:23 -0400

    Working with RoleNames instead of ID

--- Commands/Button/rulesAccepted.js
+++ Commands/Button/rulesAccepted.js
@@ -2,8 +2,7 @@
 
 module.exports.execute = async (client, interaction) => {
 	const { giveRole } = client.tools;
-	const { memberRoleId } = client.config;
 	const message = 'Thank you for confirming!\nWelcome to the server!';
-	giveRole(interaction.member, memberRoleId);
+	giveRole(interaction.member, "Member");
 	return interaction.reply({ content: message, ephemeral: true });
 };
--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,32 +1,29 @@
 module.exports = async (client, reaction, user) => {
+    const logger = client.logger;
+    const message = reaction.message;
     if (reaction.partial) {
         // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
         try {
-            client.logger.load("Loading Reaction...");
             await reaction.fetch();
-            client.logger.success("Reaction Loaded!");
         } catch (error) {
-            client.logger.error(`Something went wrong when fetching the message: ${error}`)
+            logger.error(`Something went wrong when fetching the message: ${error}`)
             // Return as some properties may be undefined/null
             return;
         }
     }
     const { reactionMessageId } = client.config;
-    const logger = client.logger;
-    const { giveRole, emojiToRoles, customEmojiIdToRoles } = client.tools;
-    const message = reaction.message;
+    const { giveRole, emojiToRoleName, customEmojiIdToRoleName } = client.tools;
 
     // Here we assign roles!
     if (message.id === reactionMessageId) {
-        let role = emojiToRoles[reaction.emoji] || customEmojiIdToRoles[reaction.emoji.id];
+        let roleName = emojiToRoleName[reaction.emoji] || customEmojiIdToRoleName[reaction.emoji.id];
 
         // There is no direct way to go from User to GuildMember
         // So what we do is access the guild that the reaction is coming from
         // and obtain the GuildMember via ID
         const member = reaction.message.guild.members.cache.get(user.id);
-        if (role) {
-            logger.event(`Gave ${user.username} role ID: ${role}`);
-            giveRole(member, role);
+        if (roleName) {
+            giveRole(member, roleName);
         }
     }
 
