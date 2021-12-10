58c583b29498596b629b155ddb22697c41562d33
KingFoofle <janngarciapagan@gmail.com>
2021-12-10 09:39:27 -0400

    Enabled Production Mode

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,21 @@
 module.exports = async (client) => {
 	client.logger.ready(`Ready! Logged in as ${client.user.tag}`);
+
+	let status, activity, type;
+	if (client.env.PRODUCTION_MODE) {
+		status = 'dnd';
+		activity = 'Foofle is performing maintenance on me.';
+		type = 'WATCHING';
+	}
+
+	else {
+		status = 'online';
+		activity = 'Feeling great! Type $help for a list of commands';
+		type = 'PLAYING';
+	}
+
+	client.user.setStatus(status);
+	client.user.setActivity(activity, { type: type });
 };
 
 module.exports.once = true;
