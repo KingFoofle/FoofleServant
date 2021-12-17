d16e3b5063a63ca0ea4adce53c917ed7581683d0
KingFoofle <janngarciapagan@gmail.com>
2021-12-16 20:25:56 -0400

    Users can only use VOICE commands in VC

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -16,17 +16,32 @@ module.exports = async (client, message) => {
 		// Shift() removes and returns the 0th index of an array
 		const commandName = args.shift();
 
-		const command = client.commands.get(commandTypes.COMMAND).get(commandName);
+		// Verify if it is a prefix command
+		let command = client.commands.get(commandTypes.COMMAND).get(commandName);
+		let isVoice;
+
+		if (!command) {
+			// Or a voice command
+			command = client.commands.get(commandTypes.VOICE).get(commandName);
+			isVoice = true;
+		}
+
 		try {
 			if (command) {
 				logger.cmd(`${user.tag} in #${message.channel.name} triggered a prefix command: ${commandName}`);
+
+				// Voice Commands can only be used in a Voice Channel
+				// TODO: Add DJ Role
+				if (isVoice) {
+					const { voice: voiceState } = message.member;
+
+					if (!voiceState || !voiceState.channel) {return message.reply('You are not connected to a voice channel!');}
+				}
 				// ...args means we unpack the array as parameters
 				command.execute(client, message, ...args);
 			}
 		}
-		catch (error) {
-			logger.error(error);
-		}
+		catch (error) {logger.error(error);}
 	}
 
 	const data = { _id: user.id, username: user.username, $inc:{ currency:1 } };
