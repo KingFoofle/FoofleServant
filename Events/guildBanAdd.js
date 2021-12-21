e0ce57f0f29c58a74b7fa60b9d4362a1247e5198
KingFoofle <janngarciapagan@gmail.com>
2021-12-21 10:10:55 -0400

    Change to then/catch

--- Events/guildBanAdd.js
+++ Events/guildBanAdd.js
@@ -7,10 +7,9 @@ module.exports = async (client, ban) => {
 	const { userSchema: userDB } = client.database;
 	const { logger } = client;
 
-	try {
-		// Remove the user from the database
-		const user = await userDB.findByIdAndRemove(ban.user.id);
-		logger.log(`Removed ${user.username} from MongoDB.`);
-	}
-	catch (err) {logger.error(err);}
+	// Remove the user from the database
+	userDB.findByIdAndRemove(ban.user.id)
+		.then(user => logger.log(`Removed ${user.username} from MongoDB.`))
+		.catch(err => logger.error(err));
+
 };
